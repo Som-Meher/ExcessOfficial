5ef0bfc4b65e339af13c36daa382bbca0dcbc079
Som_Meher <118827831+Som-Meher@users.noreply.github.com>
2023-06-16 18:21:44 +0530

    Add files via upload

---
+++ EXCESS/js/script.js
@@ -0,0 +1,19 @@
+// Sticky Navbar
+
+
+// const main_header = document.querySelector(".main-header");
+
+
+// const observer = new IntersectionObserver(
+//     () => {
+//     alert("hi");
+// }, 
+// {
+    
+//     root: null,
+//     rootMargin: "",
+//     threshold: 0,
+
+// });
+
+// observer.observe(main_header);
